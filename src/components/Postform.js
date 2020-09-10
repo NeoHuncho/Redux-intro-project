f50f1dba244282a8741abbc8f6337a63c95c480a
NeoHuncho <william.guinaudie@gmail.com>
2020-09-10 18:18:10 +0200

    added necessary components

---
+++ src/components/Postform.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class Postform extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>AddPosts</h1>
+                <form>
+                    <div>
+                        <label>Titke:</label>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Postform;
